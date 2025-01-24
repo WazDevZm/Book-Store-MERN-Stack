c474b550893c4fcd90dab96d50008d3ccaf63b07
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-24 11:13:37 +0200

    Added some comments to the text file

--- backend/config.js
+++ backend/config.js
@@ -4,3 +4,4 @@ export const mongoDBURL =
 'mongodb+srv://wazingwa:Hillcrest542!@cluster0.geyos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 //DataBase Created using MongoDB
+//Successfully ran the MongoDB database without errors, all logging correctly as expected
