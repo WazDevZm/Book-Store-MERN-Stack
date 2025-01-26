c3c6170bdf19c60c2ab00b5efb57744fd3cdca4e
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-01-26 12:48:37 +0200

    Added all files

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,25 @@ app.get('/', (request, response) => {
     // Route for Save a new Book
 app.post('./books', async(request, response) =>{
     try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message : 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title : request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+
+
 
     } catch (error) {
         console.log(error.message);
